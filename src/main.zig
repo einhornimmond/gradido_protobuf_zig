0726f2a3a183248ef2cc92368bd5b5f20c6cc6de
einhornimmond <info@einhornimmond.de>
2026-01-23 12:41:30 +0100

    load file complete into memory beforehand

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
-// const gradido_protobuf_zig = @import("gradido_protobuf_zig");
 const gradido = @import("proto/gradido.pb.zig");
 const Profiler = @import("profiler.zig").Profiler;
 
 pub fn main() !void {
+    var profiler = Profiler.init();
     const allocator = std.heap.page_allocator;
 
     const cwd = std.fs.cwd();
@@ -13,46 +13,46 @@ pub fn main() !void {
     const stat = try file.stat();
     const file_size = stat.size;
 
-    var bytes_read: u64 = 0;
+    if (file_size < 32) return error.InvalidFileLayout;
+
+    const data_size = file_size - 32; // Letzte 32 bytes = Hash
+    var file_buffer = try allocator.alloc(u8, data_size);
+    defer allocator.free(file_buffer);
+
+    _ = try file.readAll(file_buffer);
+    std.debug.print("Reading File into memory in {} ms\n", .{profiler.millis()});
+    profiler.reset();
+
+    var bytes_read: usize = 0;
     var tx_count: usize = 0;
 
-    var data_buffer: [800]u8 = undefined;
-    var profiler = Profiler.init();
+    while (bytes_read < data_size) {
+        if (data_size - bytes_read < 2) return error.InvalidFileLayout;
 
-    while (true) {
-        const remaining = file_size - bytes_read;
-
-        if (remaining == 32) {
-            // var hash: [32]u8 = undefined;
-            // try file.readAll(&hash);
-            // bytes_read += 32;
-            // std.debug.print("Final hash read (32 bytes)\n", .{});
-            break;
-        }
-
-        if (remaining < 32) {
-            return error.InvalidFileLayout;
-        }
-
-        // uint16 size lesen (little-endian)
-        var size_buf: [2]u8 = undefined;
-        _ = try file.readAll(&size_buf);
+        const size: u16 = @as(u16, @intCast(file_buffer[bytes_read])) |
+            (@as(u16, @intCast(file_buffer[bytes_read + 1])) << 8);
         bytes_read += 2;
-        const b0: u16 = @intCast(size_buf[0]);
-        const b1: u16 = @intCast(size_buf[1]);
-        const size: u16 = b0 | (b1 << 8);
 
-        // Payload lesen
-        // const buffer = try allocator.alloc(u8, size);
-        // defer allocator.free(buffer);
-        _ = try file.readAll(data_buffer[0..size]);
-        var reader: std.io.Reader = .fixed(data_buffer[0..size]);
-        var tx = try gradido.ConfirmedTransaction.decode(&reader, allocator);
-        defer tx.deinit(allocator);
+        if (data_size - bytes_read < size) return error.InvalidFileLayout;
 
+        const slice = file_buffer[bytes_read .. bytes_read + size];
         bytes_read += size;
+
+        // Slice-Reader für decode
+        var reader: std.io.Reader = .fixed(slice);
+
+        // Arena-Allocator für Submessages
+        var arena = std.heap.ArenaAllocator.init(allocator);
+        // defer arena.deinit();
+        const aallocator = arena.allocator();
+        _ = try gradido.ConfirmedTransaction.decode(&reader, aallocator);
+        // defer tx.deinit(aallocator);
+        arena.deinit(); // alle Submessages freigeben
 
         tx_count += 1;
     }
+
+    const hash_slice = file_buffer[data_size..]; // letzte 32 bytes
     std.debug.print("Done. Total transactions: {} in {} ms\n", .{ tx_count, profiler.millis() });
+    std.debug.print("Final hash slice len: {}\n", .{hash_slice.len});
 }
