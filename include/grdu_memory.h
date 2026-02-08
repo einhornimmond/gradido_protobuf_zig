9986cd545629e1f11e3324c21c95c825ff32e142
einhornimmond <silas@einhornimmond.de>
2026-02-08 20:18:15 +0100

    add missing extern C

--- include/grdu_memory.h
+++ include/grdu_memory.h
@@ -4,6 +4,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct grdu_memory {
     uint8_t* data;
     size_t last_index;
@@ -11,10 +15,14 @@ typedef struct grdu_memory {
     size_t out_of_memory_capacity; // how many bytes were needed to allocate on top of last_index, outside of capacity
 } grdu_memory;
 
-extern void grdu_memory_free(grdu_memory* mem);
+void grdu_memory_free(grdu_memory* mem);
 // return 0 on success, -1 on error
-extern int grdu_memory_init(grdu_memory* mem, size_t capacity);
-extern void grdu_memory_init_static(grdu_memory* mem, uint8_t* data, size_t capacity);
-extern uint8_t * grdu_memory_alloc(grdu_memory* mem, size_t size);
+int grdu_memory_init(grdu_memory* mem, size_t capacity);
+void grdu_memory_init_static(grdu_memory* mem, uint8_t* data, size_t capacity);
+uint8_t * grdu_memory_alloc(grdu_memory* mem, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //__GRDU_MEMORY_H
